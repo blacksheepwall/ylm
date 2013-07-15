9325e5b411c36a34ef5fa6f802f7bf75e0888c45
zhushijun <sky_810@126.com>
2013-07-15 19:33:28 +0800

    WebService测试环境配置

--- src/main/java/com/youlema/sales/ws/IHelloWorldService.java
+++
@@ -1,8 +0,0 @@
-package com.youlema.sales.ws;
-import javax.jws.WebParam;
-import javax.jws.WebService;
-
-@WebService
-public interface IHelloWorldService {
-	String sayHello(@WebParam(name = "name") String name);
-}
--- src/test/java/com/youlema/sales/testws/WsTest.java
+++ src/test/java/com/youlema/sales/testws/WsTest.java
@@ -1,25 +1,33 @@
 package com.youlema.sales.testws;
 
+import java.util.Set;
+
 import javax.annotation.Resource;
+
+import junit.framework.TestCase;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import com.youlema.sales.ws.IHelloWorldService;
-
-import junit.framework.TestCase;
+import com.yolema.tbss.ext.facade.BulletinFacade;
+import com.yolema.tbss.ext.facade.DictionaryFacade;
+import com.yolema.tbss.ext.facade.fdo.sys.DictionaryFdo;
+import com.yolema.tbss.ext.facade.result.BulletinResult;
+import com.yolema.tbss.ext.facade.result.DictionaryResult;
 
 @ContextConfiguration(locations = "file:src/test/resources/applicationContext-ws.xml")
 @RunWith(SpringJUnit4ClassRunner.class)
 public class WsTest extends TestCase {
-	@Resource
-	private IHelloWorldService client;
+    @Resource
+    BulletinFacade facade;
 	@Test
 	public void testHello() throws Exception {
-		String sayHello = client.sayHello("zhushijun");
-		assertEquals(sayHello, "zhushijun say: Hello World");
+
+        BulletinResult bulletinList = facade.queryBulletinList("BUSINESS", 100);
+
+        BulletinResult sysList = facade.queryBulletinList("SYSTEM", 100);
 	}
 	
 }
